cabbba680f40c9196e185ca5055553f02a5b6b51
Vandana-saru <vandana7402off@gmail.com>
2026-06-11 22:20:12 +0530

    added more products

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 
 console.log('hi');
-const products=[{
+/*const products=[{
     image:"images/products/athletic-cotton-socks-6-pairs.jpg",
     name:"Black and Gray Athletic Cotton Socks - 6 Pairs",
     rating:{stars:4.5,
@@ -8,17 +8,20 @@ const products=[{
     price:1090,
 },{image:"images/products/intermediate-composite-basketball.jpg",
     name:"Intermediate Size Basketball",
-    rating:{stars:4,count:127},
+    rating:{stars:4,
+      count:127},
     price:2095},
 {
     image:"images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
     name:"Adults Plain Cotton T-Shirt - 2 Pack",
-    rating:{stars:4.5,count:56
+    rating:{stars:4.5,
+      count:56
     },
     price:799
-}]
+}]*/
+let producthtml='';
 products.forEach((products)=>{
-  console.log( `   <div class="product-container">
+  producthtml+= `   <div class="product-container">
           <div class="product-image-container">
             <img class="product-image"
               src=${products.image}>
@@ -32,12 +35,13 @@ products.forEach((products)=>{
             <img class="product-rating-stars"
               src="images/ratings/rating-${products.rating.stars*10}.png">
             <div class="product-rating-count link-primary">
-              ${products.count}
+              ${products.rating.count}
             </div>
+            
           </div>
 
           <div class="product-price">
-            $${products.price/100}
+            $${(products.priceCents/100).toFixed(2)}
           </div>
 
           <div class="product-quantity-container">
@@ -65,5 +69,7 @@ products.forEach((products)=>{
           <button class="add-to-cart-button button-primary">
             Add to Cart
           </button>
-        </div>` )
+        </div>` 
 })
+//console.log(producthtml)
+document.querySelector('.products-grid').innerHTML=producthtml;
